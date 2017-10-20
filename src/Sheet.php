e41b6feb7ce6f0dcbcc8bafb5c0c7eafb22cfa65
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-10-20 16:42:53 -0200

    M  src/Sheet.php

--- src/Sheet.php
+++ src/Sheet.php
@@ -1,37 +1,74 @@
 <?php
+/**
+* Basic
+* Micro framework em PHP
+*/
+
 namespace Basic;
+
 use Box\Spout\Reader\ReaderFactory;
 use Box\Spout\Common\Type;
-class Sheet{
-    function alpha_to_num($a){
+
+/**
+ * Classe Sheet
+ */
+class Sheet
+{
+    /**
+     * Converte letras para números
+     * @param  string  $a Letra
+     * @return integer    Número
+     */
+    public function alphaToNum(string $a):integer
+    {
         $l = strlen($a);
         $n = 0;
-        for($i = 0; $i < $l; $i++)
-        $n = $n*26 + ord($a[$i]) - 0x60;
+        for ($i = 0; $i < $l; $i++) {
+            $n = $n*26 + ord($a[$i]) - 0x60;
+        }
         return $n-1;
     }
-    function num_to_alpha($n){
-        for($r = ""; $n >= 0; $n = intval($n / 26) - 1)
-        $r = chr($n%26 + 0x61) . $r;
+    /**
+     * Converte número para letra
+     * @param  integer $n Número
+     * @return string     Letra
+     */
+    public function numToAlpha(integer $n):string
+    {
+        for ($r = ""; $n >= 0; $n = intval($n / 26) - 1) {
+            $r = chr($n%26 + 0x61) . $r;
+        }
         return $r;
     }
-    function sheet_to_alpha($sheet){
+    /**
+     * Converte as chaves numéricas da planilha para chaves alfabéticas
+     * @param  array $sheet Array com chaves numéricas
+     * @return array        Array com chaves alfabéticas
+     */
+    public function sheetToAlpha(array $sheet):array
+    {
         $fixed_sheet=false;
-        foreach($sheet as $key=>$value){
-            foreach($value as $value_key=>$value_value){
+        foreach ($sheet as $key=>$value) {
+            foreach ($value as $value_key=>$value_value) {
                 unset($value[$value_key]);
-                $value[$this->num_to_alpha($value_key)]=$value_value;
+                $value[$this->numToAlpha($value_key)]=$value_value;
             }
             unset($sheet[$key]);
             $fixed_sheet[$key+1]=$value;
         }
         return $fixed_sheet;
     }
-    function to_array($sheet_name){
-        $ext=pathinfo($sheet_name,PATHINFO_EXTENSION);
-        switch($ext){
+    /**
+     * Converte uma planilha para array
+     * @param  string $sheet_name Planilha
+     * @return array              Array
+     */
+    public function toArray(string $sheet_name):array
+    {
+        $ext=pathinfo($sheet_name, PATHINFO_EXTENSION);
+        switch ($ext) {
             case 'csv':
-            $reader = ReaderFactory::create(Type::CSV); 
+            $reader = ReaderFactory::create(Type::CSV);
             break;
             case 'ods':
             $reader = ReaderFactory::create(Type::ODS);
@@ -50,13 +87,20 @@ class Sheet{
         $reader->close();
         $lists=false;
         foreach ($list['lists'] as $key => $value) {
-            $lists[$key]=$this->sheet_to_alpha($value);
+            $lists[$key]=$this->sheetToAlpha($value);
         }
         return $lists;
     }
-    function to_sheet($array,$sheet_name){
-        $ext=pathinfo($sheet_name,PATHINFO_EXTENSION);
-        switch($ext){
+    /**
+     * Converte um array de dadospara uma planilha
+     * @param  array  $array      Array de dados
+     * @param  string $sheet_name Nome da planilha
+     * @return bool               Retorna true ou false
+     */
+    public function toSheet(array $array, string $sheet_name):bool
+    {
+        $ext=pathinfo($sheet_name, PATHINFO_EXTENSION);
+        switch ($ext) {
             case 'csv':
             $writer = WriterFactory::create(Type::CSV);
             break;
@@ -68,13 +112,13 @@ class Sheet{
             break;
         }
         @$writer->openToFile($sheet_name);
-        foreach($data as $row){
+        foreach ($data as $row) {
             $writer->addRow($row);
         }
-        if(is_null($writer->close())){
-        	return true;
-        }else{
-        	return false;
+        if (is_null($writer->close())) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
